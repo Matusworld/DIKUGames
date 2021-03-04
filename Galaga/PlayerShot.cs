e8e675f2a46132b110d2dcc0210e7d7e545949f5
tossefinger <silasnostvik@hotmail.com>
2021-03-04 12:03:52 +0100

    Added Enemy and PlayerShots

---
+++ Galaga/PlayerShot.cs
@@ -0,0 +1,13 @@
+using DIKUArcade.Entities;
+using DIKUArcade.Math;
+using DIKUArcade.Graphics;
+
+namespace Galaga {
+    class PlayerShot : Entity {
+        private static Vec2F extent = new Vec2F(0.008f, 0.021f);
+        private static Vec2F direction = new Vec2F(0.0f, 0.01f);    
+
+        public PlayerShot(Vec2F position, IBaseImage image) 
+            : base(new DynamicShape(position,extent,direction), image) {}
+    }
+}
