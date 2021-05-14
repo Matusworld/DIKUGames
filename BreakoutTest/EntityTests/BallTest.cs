68f7ec1dd76fd8258a7ddd1bdc1acab3b9a513c8
uglemc <54136442+uglemc@users.noreply.github.com>
2021-05-14 10:52:35 +0200

    Added formatting and final test to BallTests.cs co authored by Silas and Mathias

--- BreakoutTest/EntityTests/BallTest.cs
+++ BreakoutTest/EntityTests/BallTest.cs
@@ -11,7 +11,7 @@ using DIKUArcade.Graphics;
 
 namespace BreakoutTest
 {
-    public class BallTest{
+    public class BallTest {
         Ball ball;
         GameEventBus eventBus;
         float tolerance;
@@ -19,50 +19,69 @@ namespace BreakoutTest
         [SetUp]
         
         public void setup() {
+
             Window.CreateOpenGLContext();
+
             ball = new Ball(
                 new DynamicShape (new Vec2F(0.45f, 0.5f), new Vec2F(0.05f,0.05f)),
                 new Image (Path.Combine(TestProjectPath.getPath(),  
                 "Assets", "Images", "ball.png")),
                 (float) Math.PI /4f);
+
             eventBus = new GameEventBus();
-            eventBus.InitializeEventBus(new List<GameEventType> { GameEventType.ControlEvent });
-            eventBus.Subscribe(GameEventType.ControlEvent, ball);
+            eventBus.InitializeEventBus(new List<GameEventType> { GameEventType.MovementEvent });
+            eventBus.Subscribe(GameEventType.MovementEvent, ball);
+
             tolerance = 0.0001f;
         }
         
         [Test]
-        public void TestRBoundaryCheckers(){
+        public void TestRBoundaryCheckers() {
+
             ball.Shape.Position.X = 1.0f;
             Assert.IsTrue(ball.RightBoundaryCheck());
         }
+
         [Test]
-        public void TestLBoundaryCheckers(){
+        public void TestLBoundaryCheckers() {
+
             ball.Shape.Position.X = 0.0f;
             Assert.IsTrue(ball.LeftBoundaryCheck());
         }
+
         [Test]
-        public void TestUBoundaryCheckers(){
+        public void TestUBoundaryCheckers() {
+
             ball.Shape.Position.Y = 1.0f;
              Assert.IsTrue(ball.UpperBoundaryCheck());
         }
+
         [Test]
-        public void TestLOBoundaryCheckers(){
+        public void TestLOBoundaryCheckers() {
+
             ball.Shape.Position.Y = -0.1f;
              Assert.IsTrue(ball.LowerBoundaryCheck());
         }
+
         [Test]
-        public void TestDirectionBoundarySetter(){
+        public void TestDirectionBoundarySetter() {   
+
             ball.Shape.Position.X = 0.0f;
             ball.Shape.Position.Y = 1.0f;
+
             ball.Shape.AsDynamicShape().Direction.X = 0.01f;
             ball.Shape.AsDynamicShape().Direction.Y = 0.01f;
+
+            float oldDirectionX = ball.Shape.AsDynamicShape().Direction.X;
+            float oldDirectionY = ball.Shape.AsDynamicShape().Direction.Y;
+
             ball.DirectionBoundarySetter();
-            Assert.IsTrue(tolerance >= Math.Abs(0.01f-ball.Shape.AsDynamicShape().Direction.X) && 
-                tolerance >= Math.Abs(0.01f-ball.Shape.AsDynamicShape().Direction.Y));
+            Assert.IsTrue(tolerance >= Math.Abs(oldDirectionX+ball.Shape.AsDynamicShape().Direction.X) && 
+                tolerance >= Math.Abs(oldDirectionY+ball.Shape.AsDynamicShape().Direction.Y));
         }
+
         [Test]
-        public void testDirectionPlayerSetter(){
+        public void testDirectionPlayerSetter( ) {
             float testTheta = ball.ReturnTheta(0.23f);
             ball.DirectionPlayerSetter(0.23f);
             Assert.IsTrue(tolerance >=Math.Abs(testTheta-1.9949113f));           
@@ -70,22 +89,45 @@ namespace BreakoutTest
             //to test for.
         }
         [Test]
-        public void testDirectionPlayerSetterLeftBounce(){
+        public void testDirectionPlayerSetterLeftBounce() {
+
             float testTheta = ball.ReturnTheta(0.0f);
             ball.DirectionPlayerSetter(0.0f);
             Assert.IsTrue(tolerance >= Math.Abs(testTheta - (3f/4f*(float)Math.PI)));
         }
         [Test]
-        public void testDirectionPlayerSetterMiddleBounce(){
+        public void testDirectionPlayerSetterMiddleBounce() {
+
             float testTheta = ball.ReturnTheta(0.5f);
             ball.DirectionPlayerSetter(0.5f);
             Assert.IsTrue(tolerance >= Math.Abs(testTheta - (2f/4f*(float)Math.PI)));
         }
         [Test]
-        public void testDirectionPlayerSetterRightBounce(){
+        public void testDirectionPlayerSetterRightBounce() {
             float testTheta = ball.ReturnTheta(1.0f);
+
             ball.DirectionPlayerSetter(1.0f);
             Assert.IsTrue(tolerance >= Math.Abs(testTheta - (1f/4f*(float)Math.PI)));
+        }
+        [Test]
+        public void testMove() {
+
+            float newPosX = 0.5f;
+            float newPosY = 0.5f;
+
+            ball.Shape.Position.X = 0.49f;
+            ball.Shape.Position.Y = 0.49f;
+
+            ball.Shape.AsDynamicShape().Direction.X = 0.01f;
+            ball.Shape.AsDynamicShape().Direction.Y = 0.01f;
+
+            eventBus.RegisterEvent( new GameEvent {
+                EventType = GameEventType.MovementEvent,
+                StringArg1 = "Move" });
+            eventBus.ProcessEvents();
+
+            Assert.IsTrue(tolerance >= Math.Abs(newPosX-ball.Shape.Position.X) && 
+                tolerance >= Math.Abs(newPosY-ball.Shape.Position.Y));
         }
     }
 }
